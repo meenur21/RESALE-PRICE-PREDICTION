ad1d13d69464b0aa781f9803a12413a57d1be956
meenur21 <147534132+meenur21@users.noreply.github.com>
2024-02-19 14:13:08 +0000

    final

--- Project_6/app.py
+++ Project_6/app.py
@@ -78,4 +78,4 @@ def predict():
             return render_template('result_new.html', predicted_price=predicted_price)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
